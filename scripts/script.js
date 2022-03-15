0d88cce918d6b88f495061bddb811ffdfc4e7e91
chrisgth <ksemkus00@gmail.com>
2022-03-15 16:50:59 +0200

    Created operate function and moved variables

--- scripts/script.js
+++ scripts/script.js
@@ -6,26 +6,46 @@
 // multiply
 // divide
 
+let sum = 0;
+let subtraction = 0;
+let multiplication = 0;
+let division = 0;
+
 function add(a, b){
-    let sum = 0;
     sum = a + b;
     return sum;
 }
 
 function subtract(a, b){
-    let subtraction = 0;
     subtraction = a - b;
     return subtraction;
 }
 
 function multiply(a, b){
-    let multiplication = 0;
     multiplication = a * b;
     return multiplication;
 }
 
-function division(a, b){
-    let division = 0;
+function divide(a, b){
     division = a / b;
     return division;
+}
+
+// Create a new function operate that takes an operator and 2 numbers 
+// and then calls one of the above functions on the numbers.
+
+function operate(operator, a, b){
+    if(operator === "+"){
+        add(a,b);
+        return sum;
+    }else if(operator === "-"){
+        subtract(a,b);
+        return subtraction;
+    }else if(operator === "*"){
+        multiply(a,b); 
+        return multiplication;
+    }else if(operator === "/"){
+        divide(a,b);
+        return division;
+    }
 }
