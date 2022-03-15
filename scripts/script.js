30b171e20dd29f53e317ff4f5636685c4fd9eb5e
chrisgth <ksemkus00@gmail.com>
2022-03-15 16:40:03 +0200

    Created simple math functions

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+// Your calculator is going to contain functions for all of the basic math operators
+// you typically find on simple calculators, 
+// so start by creating functions for the following items and testing them in your browser’s console.
+// add
+// subtract
+// multiply
+// divide
+
+function add(a, b){
+    let sum = 0;
+    sum = a + b;
+    return sum;
+}
+
+function subtract(a, b){
+    let subtraction = 0;
+    subtraction = a - b;
+    return subtraction;
+}
+
+function multiply(a, b){
+    let multiplication = 0;
+    multiplication = a * b;
+    return multiplication;
+}
+
+function division(a, b){
+    let division = 0;
+    division = a / b;
+    return division;
+}
